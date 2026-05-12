2a04d96a0b02ecbeee148f672448f48abce86788
Ebuka Abraham <45390219+buka4rill@users.noreply.github.com>
2026-05-12 14:16:41 +0200

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        // change each node's `next` to point to previous node
+        let prev = null, curr = head;
+
+        while (curr !== null) {
+            const nxt = curr.next;
+
+            // reverese the pointers
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+
+        // return new head
+        return prev;
+    }
+}
